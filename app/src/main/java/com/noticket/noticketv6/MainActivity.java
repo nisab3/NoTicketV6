3929683496e8795018daf242c8e551aaa44eec8a
nisab3 <37090969+nisab3@users.noreply.github.com>
2018-03-24 17:14:50 -0400

    ajouté les pancartes sur main activity et des spacer dans activityPancarte

--- app/src/main/java/com/noticket/noticketv6/MainActivity.java
+++ app/src/main/java/com/noticket/noticketv6/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-    // quand je bouton de nouvelle pancarte est clicker
-    // on cré un intent et lance la nouvelle activité
+    // quand le bouton de nouvelle pancarte est clicker
+    // on crée un intent et lance la nouvelle activitée
     public void ActionBoutPancarte(View view){
         Intent intent = new Intent(this, PancarteActivity.class);
         startActivity(intent);
