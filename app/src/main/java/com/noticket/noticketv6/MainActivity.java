d7592b74afcaa65e085a9e9b37a4f073793e1df0
nisab3 <37090969+nisab3@users.noreply.github.com>
2018-04-04 14:17:43 -0400

    commit poubelle?

--- app/src/main/java/com/noticket/noticketv6/MainActivity.java
+++ app/src/main/java/com/noticket/noticketv6/MainActivity.java
@@ -1,11 +1,14 @@
 package com.noticket.noticketv6;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         ImageView bp4 = findViewById(R.id.boutImPanc4);
         ImageView bp5 = findViewById(R.id.boutImPanc5);
         ImageButton bpa = findViewById(R.id.boutAjoutPanc);
+        ImageButton poubelle1 = findViewById(R.id.boutDeletePanc1);
+        ImageButton poubelle2 = findViewById(R.id.boutDeletePanc2);
+        ImageButton poubelle3 = findViewById(R.id.boutDeletePanc3);
+        ImageButton poubelle4 = findViewById(R.id.boutDeletePanc4);
+        ImageButton poubelle5 = findViewById(R.id.boutDeletePanc5);
+
+
         bp1.setOnClickListener(b);
         bp2.setOnClickListener(b);
         bp3.setOnClickListener(b);
@@ -208,23 +218,59 @@ public class MainActivity extends AppCompatActivity {
     private ImageView.OnClickListener b = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            //bouton ajoute pancarte
             if (view.getId() == R.id.boutAjoutPanc){
                 actionBoutNouveau(view);
             }
+            //bouton pancarte 1
             if (view.getId() == R.id.boutImPanc1){
                 actionBoutPancarte(view, 1);
             }
+            //bouton pancarte 2
             if (view.getId() == R.id.boutImPanc2){
                 actionBoutPancarte(view, 2);
             }
+            //bouton pancarte 3
             if (view.getId() == R.id.boutImPanc3){
                 actionBoutPancarte(view, 3);
             }
+            //bouton pancarte 4
             if (view.getId() == R.id.boutImPanc4){
                 actionBoutPancarte(view, 4);
             }
+            //bouton pancarte 5
             if (view.getId() == R.id.boutImPanc5){
                 actionBoutPancarte(view, 5);
+            }
+            // bouton delete pancarte 1
+            if (view.getId() == R.id.boutDeletePanc1){
+                findViewById(R.id.boutPanc1).setVisibility(View.GONE);
+                poteau.set_pancarte_active(1, false);
+                findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
+            }
+            // bouton delete pancarte 2
+            if (view.getId() == R.id.boutDeletePanc2){
+                findViewById(R.id.boutPanc2).setVisibility(View.GONE);
+                poteau.set_pancarte_active(2, false);
+                findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
+            }
+            // bouton delete pancarte 3
+            if (view.getId() == R.id.boutDeletePanc3){
+                findViewById(R.id.boutPanc3).setVisibility(View.GONE);
+                poteau.set_pancarte_active(3, false);
+                findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
+            }
+            // bouton delete pancarte 4
+            if (view.getId() == R.id.boutDeletePanc4){
+                findViewById(R.id.boutPanc4).setVisibility(View.GONE);
+                poteau.set_pancarte_active(4, false);
+                findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
+            }
+            // bouton delete pancarte 5
+            if (view.getId() == R.id.boutDeletePanc5){
+                findViewById(R.id.boutPanc5).setVisibility(View.GONE);
+                poteau.set_pancarte_active(5, false);
+                findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
             }
         }
     };
@@ -249,7 +295,7 @@ public class MainActivity extends AppCompatActivity {
             creeIntent(view, numero);
         }
         else{
-            Toast.makeText(getApplicationContext(),R.string.alerte_max_pancarte, Toast.LENGTH_LONG );
+            Toast.makeText(getApplicationContext(),R.string.alerte_max_pancarte, Toast.LENGTH_LONG ).show();
         }
 
 
@@ -267,35 +313,26 @@ public class MainActivity extends AppCompatActivity {
     //     int numero de la pancarte
     // out: rien
     private void creeIntent(View view, int numero){
-        Pancarte pancarte = poteau.get_pancarte(numero);
+        Pancarte pancarteUtile = poteau.get_pancarte(numero);
         Intent intent = new Intent(this, PancarteActivity.class);
         intent.putExtra("NUMPANCARTE", numero);
 
-        mettreInfo(intent, numero);     //mettre la pancarte dans le intent
-
-        startActivityForResult(intent, PANCARTE_ACTIVITY_REQUEST_CODE);
-    }
-
-    // mets les infos d'un pancarte dans un intent et le retourne
-    private Intent mettreInfo(Intent intent, int numero){
-        // prend la pancarte
-        Pancarte pancarte = poteau.get_pancarte(numero);
         // copie les infos
-        intent.putExtra("HEURE1", pancarte.getHeure(1));
-        intent.putExtra("HEURE2", pancarte.getHeure(2));
-        intent.putExtra("HEURE3", pancarte.getHeure(3));
-        intent.putExtra("JOUR1", pancarte.getJour(1));
-        intent.putExtra("JOUR2", pancarte.getJour(2));
-        intent.putExtra("JOUR3", pancarte.getJour(3));
-        intent.putExtra("MOIS", pancarte.getMois());
-        intent.putExtra("FLECHE", pancarte.getFleche());
-        intent.putExtra("IMAGE", pancarte.getImage());
-        boolean[] actif = {pancarte.heureIsActive(1), pancarte.heureIsActive(2), pancarte.heureIsActive(3),
-                            pancarte.jourIsActive(1), pancarte.jourIsActive(2), pancarte.jourIsActive(3),
-                            pancarte.moisIsActive()};
+        intent.putExtra("HEURE1", pancarteUtile.getHeure(1));
+        intent.putExtra("HEURE2", pancarteUtile.getHeure(2));
+        intent.putExtra("HEURE3", pancarteUtile.getHeure(3));
+        intent.putExtra("JOUR1", pancarteUtile.getJour(1));
+        intent.putExtra("JOUR2", pancarteUtile.getJour(2));
+        intent.putExtra("JOUR3", pancarteUtile.getJour(3));
+        intent.putExtra("MOIS", pancarteUtile.getMois());
+        intent.putExtra("FLECHE", pancarteUtile.getFleche());
+        intent.putExtra("IMAGE", pancarteUtile.getImage());
+        boolean[] actif = {pancarteUtile.heureIsActive(1), pancarteUtile.heureIsActive(2), pancarteUtile.heureIsActive(3),
+                            pancarteUtile.jourIsActive(1), pancarteUtile.jourIsActive(2), pancarteUtile.jourIsActive(3),
+                            pancarteUtile.moisIsActive()};
         intent.putExtra("ACTIVE", actif);
 
-        return intent;
+        startActivityForResult(intent, PANCARTE_ACTIVITY_REQUEST_CODE);
     }
 
     @Override
@@ -347,9 +384,33 @@ public class MainActivity extends AppCompatActivity {
         TextView t = findViewById(R.id.textImPanc1 +(numero -1));
         t.setText(resutatText);
 
+        // changer les images
+        ImageView f = findViewById(R.id.fleche1 + (numero - 1));
+        switch (fleche){
+            case 0:
+                f.setImageResource(R.drawable.fleche_pas);
+                break;
+            case 1:
+                f.setImageResource(R.drawable.fleche_g);
+                break;
+            case 2:
+                f.setImageResource(R.drawable.fleche_double);
+                break;
+            case 3:
+                f.setImageResource(R.drawable.fleche_d);
+                break;
+        }
+        ImageView im = findViewById(R.id.boutImPanc1 + (numero - 1));
+        if (image == 0){
+            im.setImageResource(R.drawable.no_parking_blank);
+        }
+        else{
+            im.setImageResource(R.drawable.no_stop_blank);
+        }
+
         // faire apparaitre la pancarte et l'activer et la mettre dans le poteau
         poteau.set_pancarte_active(numero, true);
-        poteau.set_pancarte(pancarteRetour, numero);
+
         findViewById(R.id.boutPanc1 + (numero -1) ).setVisibility(View.VISIBLE);
         // cacher le bouton ajouterPancarte si il y a maintenant 5 pancarte
         cacheBoutAjoute();
@@ -396,7 +457,7 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.boutAjoutPanc).setVisibility(View.VISIBLE);
         }
     }
-    
+
     // désactivation du back button car lorqu'on reviens il repart a zero
     // il faut donc utiliser le home button pour sortir
     @Override
