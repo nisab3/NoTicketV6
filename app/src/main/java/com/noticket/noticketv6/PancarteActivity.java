eab317e7309158b91749ae47a607cfc224608bca
nisab3 <37090969+nisab3@users.noreply.github.com>
2018-04-05 19:07:29 -0400

    changer le visuel et les icons

--- app/src/main/java/com/noticket/noticketv6/PancarteActivity.java
+++ app/src/main/java/com/noticket/noticketv6/PancarteActivity.java
@@ -57,11 +57,11 @@ public class PancarteActivity extends AppCompatActivity {
 
     // bouton cancel / ok
     private FloatingActionButton ok;
-    private Button can;
+    private FloatingActionButton can;
 
     // bouton favorie
-    private Button allerFav;
-    private ImageButton ajoutFav;
+    private FloatingActionButton allerFav;
+    private FloatingActionButton ajoutFav;
 
     // imageSwitcher fleche
     private ImageSwitcher imfleche;
@@ -158,7 +158,7 @@ public class PancarteActivity extends AppCompatActivity {
 
         if ( requestCode == FAVORIE_ACTIVITY_REQUEST_CODE){
             if (resultCode == RESULT_OK){
-                ajoutFav.setImageResource(android.R.drawable.star_big_on);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_jaune);
                 setInfoStart(data);
             }
         }
@@ -171,7 +171,7 @@ public class PancarteActivity extends AppCompatActivity {
 
             if (view.getId() == R.id.boutAjoutFavorie){
                 sauvegardeFavorie();
-                ajoutFav.setImageResource(android.R.drawable.star_big_on);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_jaune);
             }
 
             if (view.getId() == R.id.boutFavorie){
@@ -191,7 +191,7 @@ public class PancarteActivity extends AppCompatActivity {
                     noImageP = 0;
                 }
                 setImageParking(noImageP);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
 
             }
             // bouton gauche pour changer l'image pancarte
@@ -203,7 +203,7 @@ public class PancarteActivity extends AppCompatActivity {
                     noImageP = 1;
                 }
                 setImageParking(noImageP);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             // bouton droite pour changer la fleche
             if (view.getId() == R.id.boutFlecheD){
@@ -214,7 +214,7 @@ public class PancarteActivity extends AppCompatActivity {
                     noFleche++;
                 }
                 setFleche(noFleche);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             // bouton gauche pour changer la fleche
             if (view.getId() == R.id.boutFlecheG){
@@ -225,7 +225,7 @@ public class PancarteActivity extends AppCompatActivity {
                     noFleche--;
                 }
                 setFleche(noFleche);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             // bouton ajouter ligne heure
             // trouve une ligne non-active
@@ -330,49 +330,49 @@ public class PancarteActivity extends AppCompatActivity {
                 findViewById(R.id.layoutHeure1).setVisibility(View.GONE);
                 findViewById(R.id.boutAjoutHeure).setVisibility(View.VISIBLE);
                 pancarte.heureSetActive(false, 1);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             // delete ligne heure 2
             if (view.getId() == R.id.boutDeleteH2){
                 findViewById(R.id.layoutHeure2).setVisibility(View.GONE);
                 pancarte.heureSetActive(false, 2);
                 findViewById(R.id.boutAjoutHeure).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             //delete ligne heure 3
             if (view.getId() == R.id.boutDeleteH3){
                 findViewById(R.id.layoutHeure3).setVisibility(View.GONE);
                 pancarte.heureSetActive(false, 3);
                 findViewById(R.id.boutAjoutHeure).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             //delete ligne jour1
             if (view.getId() == R.id.boutDeleteJ1){
                 findViewById(R.id.layoutJour1).setVisibility(View.GONE);
                 pancarte.jourSetActive(false, 1);
                 findViewById(R.id.boutAjoutJour).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             //delete ligne jour 2
             if (view.getId() == R.id.boutDeleteJ2){
                 findViewById(R.id.layoutJour2).setVisibility(View.GONE);
                 pancarte.jourSetActive(false, 2);
                 findViewById(R.id.boutAjoutJour).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             //delete ligne jour 3
             if (view.getId() == R.id.boutDeleteJ3){
                 findViewById(R.id.layoutJour3).setVisibility(View.GONE);
                 pancarte.jourSetActive(false, 3);
                 findViewById(R.id.boutAjoutJour).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
             //delete ligne mois 1
             if (view.getId() == R.id.boutDeleteM1){
                 findViewById(R.id.layoutMois1).setVisibility(View.GONE);
                 pancarte.moisSetActive(false);
                 findViewById(R.id.boutAjoutMois).setVisibility(View.VISIBLE);
-                ajoutFav.setImageResource(android.R.drawable.star_big_off);
+                ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
             }
         }
     };
@@ -521,7 +521,7 @@ public class PancarteActivity extends AppCompatActivity {
             }
 
         }
-        ajoutFav.setImageResource(android.R.drawable.star_big_off);
+        ajoutFav.setImageResource(R.drawable.icon_etoile_vide);
         fragmentTransaction.remove(frag);
         fragmentTransaction.commit();
     }
