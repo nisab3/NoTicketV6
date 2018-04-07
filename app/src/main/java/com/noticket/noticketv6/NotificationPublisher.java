eacc054722abc5250b34190a340c29590b454529
nisab3 <37090969+nisab3@users.noreply.github.com>
2018-04-06 21:20:52 -0400

    regler encore les probelmes de favorie

---
+++ app/src/main/java/com/noticket/noticketv6/NotificationPublisher.java
@@ -0,0 +1,33 @@
+package com.noticket.noticketv6;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * créateur BrandonSmith
+ * trouvé dans un tutoriel en ligne sur les notifications à délai
+ * adresse URL :
+ * https://gist.github.com/BrandonSmith/6679223
+ */
+
+
+
+
+public class NotificationPublisher extends BroadcastReceiver {
+
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(id, notification);
+
+    }
+}
