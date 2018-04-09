07859c3530b7ab3437b683134acef3f181179048
tommy127127 <tommy_127@hotmail.com>
2018-04-09 19:53:37 -0400

    Transformation de plusieurs string en @string.xml

--- app/src/main/java/com/noticket/noticketv6/MainActivity.java
+++ app/src/main/java/com/noticket/noticketv6/MainActivity.java
@@ -136,9 +136,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-
-
-
         return true;
     }
 
@@ -523,7 +520,7 @@ public class MainActivity extends AppCompatActivity {
         Button boutonAlarme = (Button) analyseView.findViewById(R.id.boutonFavAnuler);
         Button boutonRetour = (Button) analyseView.findViewById(R.id.boutonFavSup);
 
-        TextView reponse = (TextView) analyseView.findViewById(R.id.titre);
+        TextView reponse = (TextView) analyseView.findViewById(R.id.titreAvertissement);
         TextView momentDisponible = (TextView) analyseView.findViewById(R.id.momentDisponible);
         TextView tempsDisponible = (TextView) analyseView.findViewById(R.id.tempsRestant);
 
