b5b5602d59c63bc00d75fa7e66d534d9be729b82
nisab3 <37090969+nisab3@users.noreply.github.com>
2018-04-08 23:35:04 -0400

    update de tutoriel

---
+++ app/src/main/java/com/noticket/noticketv6/TutorielPancarte.java
@@ -0,0 +1,13 @@
+package com.noticket.noticketv6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorielPancarte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutoriel_pancarte);
+    }
+}
